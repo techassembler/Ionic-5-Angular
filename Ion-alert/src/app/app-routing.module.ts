81c44d15bfd8d341cb36ed54648b5aacb5eb3572
techassembler44 <techassembler44@gmail.com>
2020-10-04 09:54:15 +0530

    ion-alert

---
+++ Ion-alert/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'alert',
+    pathMatch: 'full'
+  },
+  {
+    path: 'alert',
+    loadChildren: () => import('./alert/alert.module').then(m => m.AlertPageModule)
+  },
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
